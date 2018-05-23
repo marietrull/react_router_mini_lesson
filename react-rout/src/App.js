6ce05df489929d67878caaacff7df0fff03a9c2e
marietrull <marietrull@gmail.com>
2018-05-23 12:23:57 -0500

    Created PageOne and PageTwo Components + imported tools from react-router-dom

--- react-rout/src/App.js
+++ react-rout/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+
+// Creating components in one page to save time
+// Would normally render each component in a seperate file
+const PageOneContainer = () => {
+  return (
+
+    <div>
+      Page one
+    </div>
+
+    )  
+}
+
+const PageTwoContainer = () => {
+  return (
+
+    <div>
+      Page Two
+    </div>
+    
+    )  
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
